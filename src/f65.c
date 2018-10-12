1e448c0254e2b768f00b1eef8a3c236984b269d1
LAK132 <lakridesagain@gmail.com>
2018-10-12 17:16:17 +1030

    Rename some variables

--- src/f65.c
+++ src/f65.c
@@ -11,25 +11,49 @@
 */
 uint8_t x, y;
 uint32_t i, j, k;
-uint32_t prev_font = 0; // read only
-uint16_t glyph_count = 0;
-uint16_t tile_map_start = 0;
-uint32_t point_list = 0;
-uint32_t tile_array_start = 0U;
-uint32_t tile_map_size = 0U, cards = 0U;
-uint32_t point_list_length = 0U;
-uint32_t map = 0, tile = 0;
-uint32_t screen = 0, colour = 0;
+uint32_t prev_font = 0U; // read only
+uint16_t glyph_count = 0U;
+
+// point list
+uint32_t point_list_address = 0U, point_list_size = 0U;
+
+// tile map
+uint16_t tile_map_start = 0U;
+uint32_t tile_map_address = 0U, tile_map_size = 0U;
+
+// tile array
+uint32_t tile_array_start = 0U, tile_array_address = 0U;
+
+uint32_t screen = 0U, colour = 0U;
 uint8_t bytes_per_row;
 uint8_t rows_above;
 uint8_t rows_below;
 uint32_t map_pos, array_pos;
 uint32_t glyph_height, glyph_width, glyph_size;
 uint8_t trim_pixels;
-uint16_t the_code_point = 0, card;
+uint16_t the_code_point = 0U;
 
 uint8_t clear_pattern[4] = {0x20, 0x00, 0x20, 0x00};
 uint8_t end_of_line_pattern[2] = {0xff, 0xff};
+// uint8_t gradient[] = {0x0, 0x1F, 0x3F, 0x5F, 0x9F, 0xBF, 0xDF, 0xFF};
+uint8_t gradient[] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+    0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+    0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+    0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+    0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
+    0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
+    0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
+    0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57,
+    0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
+    0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
+    0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
+    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77,
+    0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
+};
 
 void renderTextUTF16(uint32_t font_address, uint16_t *string, render_buffer_t *scratch,
                     uint8_t colour_and_attributes, uint8_t alpha_and_extras)
@@ -102,12 +126,13 @@ void findFontStructures(uint32_t font_address)
     lcopy(font_address + 0x80, (long)&glyph_count, 2);
 
     lcopy(font_address + 0x82, (long)&tile_map_start, 2);
-    point_list_length = tile_map_start - 0x100;
-    point_list = font_address + 0x100;
+    point_list_size = tile_map_start - 0x100;
+    point_list_address = font_address + 0x100;
     tile_array_start = 0;
     lcopy(font_address + 0x84, (long)&tile_array_start, 3);
     tile_map_size = font_address + tile_map_start - tile_array_start;
-    map = font_address + tile_map_start;
+    tile_map_address = font_address + tile_map_start;
+    tile_array_address = font_address + tile_array_start;
 }
 
 void renderGlyph(uint32_t font_address, uint16_t code_point, render_buffer_t *b, uint8_t colour_and_attributes, uint8_t alpha_and_extras)
@@ -123,15 +148,15 @@ void renderGlyph(uint32_t font_address, uint16_t code_point, render_buffer_t *b,
 
     // XXX - Code points are in numerical order, so speed this up with
     // a binary search.
-    for (i = 0; i < point_list_length; i += 5)
+    for (i = 0; i < point_list_size; i += 5)
     {
-        lcopy(point_list + i, (long)&the_code_point, 2);
+        lcopy(point_list_address + i, (long)&the_code_point, 2);
         if (the_code_point != code_point)
             continue;
 
         // We have the glyph, so dig out the information on it.
         map_pos = 0;
-        lcopy(point_list + i + 2, (long)&map_pos, 3);
+        lcopy(point_list_address + i + 2, (long)&map_pos, 3);
 
         rows_above = lpeek(map_pos);
         rows_below = lpeek(map_pos + 1);
@@ -185,14 +210,14 @@ void renderGlyph(uint32_t font_address, uint16_t code_point, render_buffer_t *b,
             // attributes (blink, underline etc)
             // This requires 0x20 for the first byte, to enable alpha blending
             lpoke(colour, alpha_and_extras);
-            if (y == rows_above-1)
+            if (y == rows_above - 1)
                 lpoke(colour + 1, colour_and_attributes);
             else
                 lpoke(colour + 1, colour_and_attributes & (~ATTRIB_UNDERLINE));
             if (bytes_per_row > 2)
             {
                 lpoke(colour + 2, alpha_and_extras);
-                if (y == rows_above-1)
+                if (y == rows_above - 1)
                     lpoke(colour + 3, colour_and_attributes);
                 else
                     lpoke(colour + 3, colour_and_attributes & (~ATTRIB_UNDERLINE));
@@ -220,22 +245,25 @@ void renderGlyph(uint32_t font_address, uint16_t code_point, render_buffer_t *b,
         screen = b->screen_ram + b->columns_used + b->columns_used - 1;
         for (y = 0; y < 30; ++y)
         {
-	  lpoke(screen, (lpeek(screen) & 0x1f) | trim_pixels);
-	  screen += 200;
+            lpoke(screen, (lpeek(screen) & 0x1f) | trim_pixels);
+            screen += 200;
         }
 
         break;
     }
-    if (i == point_list_length)
+    if (i == point_list_size)
     {
         // Unknown Glyph, so draw a box with the code point number in it?
     }
 }
 
+uint32_t point_tile, tile_address, tile_cards;
+uint16_t card_address;
 void patchFont(uint32_t font_address)
 {
-    if (prev_font != font_address)
-        findFontStructures(font_address);
+    // if (prev_font == font_address) return;
+
+    findFontStructures(font_address);
 
     // Patch tile_array_start
     tile_array_start += font_address;
@@ -243,24 +271,30 @@ void patchFont(uint32_t font_address)
 
     tile_array_start /= 0x40;
 
-    for (i = 0; i < point_list_length; i += 5)
+    for (i = 0; i < point_list_size; i += 5)
     {
-        map_pos = 0;
-        lcopy(point_list + i + 2, (long)&map_pos, 3);
-        tile = map + map_pos;
-        lcopy((long)&tile, point_list + i + 2, 3);
-        cards = tile + 4;
+        // map_pos = 0;
+        // lcopy(point_list + i + 2, (long)&map_pos, 3);
+        // tile = map + map_pos;
+        tile_address = 0;
+        point_tile = point_list_address + i + 2;
+        lcopy(point_tile, (long)&tile_address, 3);
+        tile_address += tile_map_address;
+        lcopy((long)&tile_address, point_tile, 3);
 
-        rows_above = lpeek(tile);
-        rows_below = lpeek(tile + 1);
-        bytes_per_row = lpeek(tile + 2);
+        rows_above = lpeek(tile_address);
+        rows_below = lpeek(tile_address + 1);
+        bytes_per_row = lpeek(tile_address + 2);
+        // tile_address + 3 -> trim_pixels
+        tile_cards = tile_address + 4;
         glyph_size = (int)(rows_above + rows_below) * bytes_per_row;
         for (j = 0; j < glyph_size; ++j)
         {
-            lcopy(cards, (long)&card, 2);
-            card += tile_array_start;
-            lcopy((long)&card, cards, 2);
-            cards += 2;
+            lcopy(tile_cards, (long)&card_address, 2);
+            // lcopy(gradient, (((uint32_t)card_address&0x0FFF) * 64) + tile_array_address, 64);
+            card_address += tile_array_start;
+            lcopy((long)&card_address, tile_cards, 2);
+            tile_cards += 2;
         }
     }
 }
