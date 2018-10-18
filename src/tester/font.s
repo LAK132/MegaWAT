c99b7df90cc352884096d949813d2c87abc215dc
LAK132 <lakridesagain@gmail.com>
2018-10-18 16:41:06 +1030

    Fix undefined references

--- src/tester/font.s
+++ src/tester/font.s
@@ -1,6 +1,7 @@
-global font_data
-global after_font_data
-
+    global font_file
+    global font_file_size
+font_file: dd font_data
+font_file_size: dd after_font_data-font_data
 font_data:
-    incbin "../../example.f65"
+    incbin "../../example.f65",2
 after_font_data:
