1fceb8440b1d30d66fca4b575f46b0a1cec137f4
LAK132 <lakridesagain@gmail.com>
2018-10-25 15:37:06 +1030

    Formatting

--- src/f65.c
+++ src/f65.c
@@ -264,71 +264,78 @@ void renderGlyph(uint16_t code_point, uint8_t colour_and_attributes, uint8_t alp
 
     // Make space if this glyph is not at the end
     start_column = (position < active_rbuffer->glyph_count)
-                       ? active_rbuffer->glyphs[position].first_column
-                       : active_rbuffer->columns_used;
+        ? active_rbuffer->glyphs[position].first_column
+        : active_rbuffer->columns_used;
 
     if (start_column > 99)
         return;
 
     // XXX - Code points are in numerical order, so speed this up with
     // a binary search.
-    if (font_id) {
-      for (i = 0; i < point_list_size; i += 5)
-	{
-	  lcopy(point_list_address + i, (ptr_t)&the_code_point, 2);
-	  if (the_code_point == code_point)
-            break;
-	}
-      // No such glyph
-      if (the_code_point != code_point) {
-	// XXX We could instead draw a hollow box to mean unknown unicode point
-	return;
-      }
-    } else {
-      // Font ID 0 = C64 character ROM font
-      // Valid only for 0x00 - 0xFF
-      if (code_point>0xFF) return;
+    if (font_id)
+    {
+        for (i = 0; i < point_list_size; i += 5)
+        {
+            lcopy(point_list_address + i, (ptr_t)&the_code_point, 2);
+            if (the_code_point == code_point)
+                break;
+        }
+        // No such glyph
+        if (the_code_point != code_point)
+        {
+            // XXX We could instead draw a hollow box to mean unknown unicode point
+            return;
+        }
+    }
+    else
+    {
+        // Font ID 0 = C64 character ROM font
+        // Valid only for 0x00 - 0xFF
+        if (code_point>0xFF) return;
     }
 
     // We have the glyph, so dig out the information on it.
-    if (font_id) {
-      map_pos = 0;
-      lcopy(point_list_address + i + 2, (ptr_t)&map_pos, 3);
-      
-      rows_above = lpeek(map_pos);
-      rows_below = lpeek(map_pos + 1);
-      bytes_per_row = lpeek(map_pos + 2);
-      trim_pixels = lpeek(map_pos + 3);
-    } else {
-      // C64 character ROM, so always 1x1 bytes, no trim.
-      rows_above=1; rows_below=0; bytes_per_row=1; trim_pixels=0;
+    if (font_id)
+    {
+        map_pos = 0;
+        lcopy(point_list_address + i + 2, (ptr_t)&map_pos, 3);
+
+        rows_above = lpeek(map_pos);
+        rows_below = lpeek(map_pos + 1);
+        bytes_per_row = lpeek(map_pos + 2);
+        trim_pixels = lpeek(map_pos + 3);
     }
-    
+    else
+    {
+        // C64 character ROM, so always 1x1 bytes, no trim.
+        rows_above=1; rows_below=0; bytes_per_row=1; trim_pixels=0;
+    }
+
     // If glyph is 0 pixels wide, nothing to do.
     if (!bytes_per_row)
-      return;
+        return;
 
     // If glyph would overrun the buffer, don't draw it.
     if ((bytes_per_row + active_rbuffer->columns_used) > 99)
-      return;
-    
+        return;
+
     // Don't allow glyphs that go too far above base line
     if (rows_above >= active_rbuffer->baseline_row)
-      return;
+        return;
     if (rows_below >= (30 - active_rbuffer->baseline_row))
-      return;
-    
+        return;
+
     // Ok, we have a glyph, so now we make space in the glyph list
     // This is an overlapping copy, so we have to copy to a temp location first
     if (position < active_rbuffer->glyph_count)
-      {
-	// Update the first_column field of each of the shoved glyphs
-	for (y = position; y < active_rbuffer->glyph_count; y++)
-	  active_rbuffer->glyphs[y].first_column += bytes_per_row;
-	lcopy_safe((ptr_t)&active_rbuffer->glyphs[position], (ptr_t)&active_rbuffer->glyphs[position + 1],
-		   sizeof(glyph_details_t) * (active_rbuffer->glyph_count - position));
-      }
-    
+    {
+        // Update the first_column field of each of the shoved glyphs
+        for (y = position; y < active_rbuffer->glyph_count; y++)
+        active_rbuffer->glyphs[y].first_column += bytes_per_row;
+        lcopy_safe((ptr_t)&active_rbuffer->glyphs[position], (ptr_t)&active_rbuffer->glyphs[position + 1],
+            sizeof(glyph_details_t) * (active_rbuffer->glyph_count - position));
+    }
+
     // Record details about this glyph
     gd = &active_rbuffer->glyphs[position];
     gd->code_point = code_point;
@@ -339,113 +346,114 @@ void renderGlyph(uint16_t code_point, uint8_t colour_and_attributes, uint8_t alp
     gd->trim_pixels = trim_pixels;
     gd->first_column = start_column;
     gd->colour_and_attributes = colour_and_attributes;
-    
+
     bytes_per_row = bytes_per_row << 1;
-    
+
     // Skip header
     map_pos += 4;
-    
+
     if (position < active_rbuffer->glyph_count)
-      {
-	// Char is not on the end, so make space
-	
-	// First, work out the address of the start row and column
-	screen = active_rbuffer->screen_ram + start_column * char_size;
-	colour = active_rbuffer->colour_ram + start_column * char_size;
-	for (y = active_rbuffer->max_above; y < active_rbuffer->baseline_row; ++y)
-	  {
-	    screen += screen_width;
-	    colour += screen_width;
-	  }
-	
-	// Now we need to copy each row of data to the screen RAM and colour RAM buffers
-	for (y = 0; y < (active_rbuffer->max_above + active_rbuffer->max_below); ++y)
-	  {
-	    // Make space
-	    lcopy_safe(screen, screen + bytes_per_row, (active_rbuffer->columns_used - start_column) * 2);
-	    lcopy_safe(colour, colour + bytes_per_row, (active_rbuffer->columns_used - start_column) * 2);
-	    
-	    // Fill screen and colour RAM with empty patterns
-	    lcopy((ptr_t)&clear_pattern, screen, bytes_per_row);
-	    lcopy(screen, screen + 2, bytes_per_row - 2);
-	    lfill(colour, 0x00, bytes_per_row);
-	    
-	    screen += screen_width;
-	    colour += screen_width;
-	  }
-      }
-    
+    {
+        // Char is not on the end, so make space
+
+        // First, work out the address of the start row and column
+        screen = active_rbuffer->screen_ram + start_column * char_size;
+        colour = active_rbuffer->colour_ram + start_column * char_size;
+        for (y = active_rbuffer->max_above; y < active_rbuffer->baseline_row; ++y)
+        {
+            screen += screen_width;
+            colour += screen_width;
+        }
+
+        // Now we need to copy each row of data to the screen RAM and colour RAM buffers
+        for (y = 0; y < (active_rbuffer->max_above + active_rbuffer->max_below); ++y)
+        {
+            // Make space
+            lcopy_safe(screen, screen + bytes_per_row, (active_rbuffer->columns_used - start_column) * 2);
+            lcopy_safe(colour, colour + bytes_per_row, (active_rbuffer->columns_used - start_column) * 2);
+
+            // Fill screen and colour RAM with empty patterns
+            lcopy((ptr_t)&clear_pattern, screen, bytes_per_row);
+            lcopy(screen, screen + 2, bytes_per_row - 2);
+            lfill(colour, 0x00, bytes_per_row);
+
+            screen += screen_width;
+            colour += screen_width;
+        }
+    }
+
     // Work out first address of screen and colour RAM
-    
+
     // First, work out the address of the start row and column
     screen = active_rbuffer->screen_ram + start_column * char_size;
     colour = active_rbuffer->colour_ram + start_column * char_size;
     for (y = rows_above; y < active_rbuffer->baseline_row; ++y)
-      {
-	screen += screen_width;
-	colour += screen_width;
-      }
-    
+    {
+        screen += screen_width;
+        colour += screen_width;
+    }
+
     // Now we need to copy each row of data to the screen RAM and colour RAM buffers
     for (y = 0; y < (rows_above + rows_below); ++y)
-      {
-	// Copy screen data
-	if (font_id)
-	  lcopy((ptr_t)map_pos, (ptr_t)screen, bytes_per_row);
-	else {
-	  // C64 character ROM, so just write code point in low byte, and empty high byte
-	  lpoke(screen,code_point);
-	  lpoke(screen+1,0x00);
-	}
-	
-	
-	// Fill colour RAM.
-	// Bit 5 of low byte is alpha blending flag. This is true for font glyphs, and false
-	// for graphics tiles.
-	// Then bottom 4 bits of high byte is foreground colour, and the upper 4 bits are VIC-III
-	// attributes (blink, underline etc)
-	// This requires 0x20 for the first byte, to enable alpha blending
-	lpoke(colour, alpha_and_extras);
-	if (y == rows_above - 1)
-	  lpoke(colour + 1, colour_and_attributes);
-	else
-	  lpoke(colour + 1, colour_and_attributes & (~ATTRIB_UNDERLINE));
-	if (bytes_per_row > 2)
-	  {
-	    lpoke(colour + 2, alpha_and_extras);
-	    if (y == rows_above - 1)
-	      lpoke(colour + 3, colour_and_attributes);
-	    else
-	      lpoke(colour + 3, colour_and_attributes & (~ATTRIB_UNDERLINE));
-	  }
-	// Then use DMA to copy the pair of bytes out
-	// (DMA timing is a bit funny, hence we need to have the two copies setup above
-	//  for this to work reliably).
-	if (bytes_per_row > 4)
-	  lcopy((ptr_t)colour, (ptr_t)colour + 2, bytes_per_row - 2);
-	
-	map_pos += bytes_per_row;
-	
-	screen += screen_width;
-	colour += screen_width;
-      }
-    
+    {
+        // Copy screen data
+        if (font_id)
+            lcopy((ptr_t)map_pos, (ptr_t)screen, bytes_per_row);
+        else
+        {
+            // C64 character ROM, so just write code point in low byte, and empty high byte
+            lpoke(screen,code_point);
+            lpoke(screen+1,0x00);
+        }
+
+
+        // Fill colour RAM.
+        // Bit 5 of low byte is alpha blending flag. This is true for font glyphs, and false
+        // for graphics tiles.
+        // Then bottom 4 bits of high byte is foreground colour, and the upper 4 bits are VIC-III
+        // attributes (blink, underline etc)
+        // This requires 0x20 for the first byte, to enable alpha blending
+        lpoke(colour, alpha_and_extras);
+        if (y == rows_above - 1)
+            lpoke(colour + 1, colour_and_attributes);
+        else
+            lpoke(colour + 1, colour_and_attributes & (~ATTRIB_UNDERLINE));
+        if (bytes_per_row > 2)
+        {
+            lpoke(colour + 2, alpha_and_extras);
+            if (y == rows_above - 1)
+                lpoke(colour + 3, colour_and_attributes);
+            else
+                lpoke(colour + 3, colour_and_attributes & (~ATTRIB_UNDERLINE));
+        }
+        // Then use DMA to copy the pair of bytes out
+        // (DMA timing is a bit funny, hence we need to have the two copies setup above
+        //  for this to work reliably).
+        if (bytes_per_row > 4)
+            lcopy((ptr_t)colour, (ptr_t)colour + 2, bytes_per_row - 2);
+
+        map_pos += bytes_per_row;
+
+        screen += screen_width;
+        colour += screen_width;
+    }
+
     active_rbuffer->glyph_count++;
     active_rbuffer->columns_used += (bytes_per_row >> 1);
     if (rows_above > active_rbuffer->max_above)
-      active_rbuffer->max_above = rows_above;
+        active_rbuffer->max_above = rows_above;
     if (rows_below > active_rbuffer->max_below)
-      active_rbuffer->max_below = rows_below;
-    
+        active_rbuffer->max_below = rows_below;
+
     // then apply trim to entire column
     trim_pixels = trim_pixels << 5;
     screen = active_rbuffer->screen_ram + start_column + start_column + bytes_per_row - 1;
     for (y = 0; y < 30; ++y)
-      {
-	lpoke(screen, (lpeek(screen) & 0x1f) | trim_pixels);
-	screen += screen_width;
-      }
-    
+    {
+        lpoke(screen, (lpeek(screen) & 0x1f) | trim_pixels);
+        screen += screen_width;
+    }
+
 }
 
 ptr_t font_address = 0;
