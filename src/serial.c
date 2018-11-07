7452e569a4e36f184c2f241cf5d55f8d0393cf1e
LAK132 <lakridesagain@gmail.com>
2018-11-07 16:28:09 +1030

    Add integer printout with serial

--- src/serial.c
+++ src/serial.c
@@ -1,5 +1,7 @@
 #include "serial.h"
 
+uint8_t console_indent = 0;
+
 void console_write_pchar(uint8_t c)
 {
     __asm__("lda (sp)");
@@ -7,10 +9,13 @@ void console_write_pchar(uint8_t c)
     __asm__("nop");
 }
 
+int32_t ii;
 void console_write_pstr(uint8_t *str)
 {
-    for (m = 0; str[m]; ++m)
-        console_write_pchar(str[m]);
+    for (ii = 0; ii < console_indent; ++ii)
+        console_write_pchar(' ');
+    for (ii = 0; str[ii]; ++ii)
+        console_write_pchar(str[ii]);
 }
 
 void console_write_achar(uint8_t c)
@@ -22,7 +27,7 @@ void console_write_achar(uint8_t c)
         __asm__("sta $d643");
         __asm__("nop");
     }
-    else if (c >= 0x30)
+    else if (c > 0x40)
     {
         __asm__("lda (sp)");
         __asm__("adc #$1F");
@@ -39,6 +44,51 @@ void console_write_achar(uint8_t c)
 
 void console_write_astr(uint8_t *str)
 {
-    for (m = 0; str[m]; ++m)
-        console_write_achar(str[m]);
+    for (ii = 0; ii < console_indent; ++ii)
+        console_write_achar(' ');
+    for (ii = 0; str[ii]; ++ii)
+        console_write_achar(str[ii]);
+}
+
+uint8_t cc[8];
+void console_write_au8(uint8_t n)
+{
+    console_write_achar(' ');
+    console_write_achar('0');
+    console_write_achar('x');
+    for (ii = 0; ii < 2; ++ii)
+        cc[ii] = (n >> (ii * 4)) & 0xF;
+    for (--ii; ii >= 0; --ii)
+        if (cc[ii] >= 0xA)
+            console_write_achar('A' + (cc[ii] - 0xA));
+        else
+            console_write_achar('0' + cc[ii]);
+}
+
+void console_write_au16(uint16_t n)
+{
+    console_write_achar(' ');
+    console_write_achar('0');
+    console_write_achar('x');
+    for (ii = 0; ii < 4; ++ii)
+        cc[ii] = (n >> (ii * 4)) & 0xF;
+    for (--ii; ii >= 0; --ii)
+        if (cc[ii] >= 0xA)
+            console_write_achar('A' + (cc[ii] - 0xA));
+        else
+            console_write_achar('0' + cc[ii]);
+}
+
+void console_write_au32(uint32_t n)
+{
+    console_write_achar(' ');
+    console_write_achar('0');
+    console_write_achar('x');
+    for (ii = 0; ii < 8; ++ii)
+        cc[ii] = (n >> (ii * 4)) & 0xF;
+    for (--ii; ii >= 0; --ii)
+        if (cc[ii] >= 0xA)
+            console_write_achar('A' + (cc[ii] - 0xA));
+        else
+            console_write_achar('0' + cc[ii]);
 }
--- src/serial.h
+++ src/serial.h
@@ -1,8 +1,15 @@
-#include "globals.h"
+#include "megaint.h"
 #include "memory.h"
+
+extern uint8_t console_indent;
 
 void console_write_pchar(uint8_t c);
 void console_write_pstr(uint8_t *str);
+
 void console_write_achar(uint8_t c);
 void console_write_astr(uint8_t *str);
+void console_write_au8(uint8_t n);
+void console_write_au16(uint16_t n);
+void console_write_au32(uint32_t n);
+
 #define console_write_newline() console_write_astr("\r\n")
