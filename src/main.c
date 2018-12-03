e48a766eb5930601ada320068aab38f5f2f9d2b4
LAK132 <lakridesagain@gmail.com>
2018-12-03 15:07:24 +1030

    Update main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
   A simple slide presentation program for the MEGA65.
-
+  Written by Lucas "LAK132" Kleiss and Paul Gardner-Stephen
 */
 
 #include "main.h"
@@ -11,33 +11,20 @@ void main(void)
 void megamain()
 #endif
 {
-    // /*
     m65_io_enable();
-    // disable keyboard interupt
-    // POKE(0x0314, 0x81);
-    // disable all maskable interupt
-    __asm__("sei");
-    videoDisableVicIIHotReg();
 
     // Turn off write protection so we can use more RAM
     toggle_write_protection();
-
-    // while (1)
-    //     POKE(0xd020U, PEEK(0xd020U)+1);
 
     // Then patch the pointers in the font(s) to be correct
     current_font = ASSET_RAM;
     patchFonts();
     setFont(0);
 
-    editor_initialise();
-
-    editor_show_cursor();
+    editor_start();
 
     #ifdef __MEGA65__
     for (;;)
-        // continue;
-        editor_poll_keyboard();
     #endif
-    // */
+        editor_poll_keyboard();
 }
